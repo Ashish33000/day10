92505c8f091e0a3cf92138264a834af5af6ce87e
Ashish Kumar <ashishbhagat330@gmail.com>
2024-04-17 16:58:04 +0530

    added

---
+++ ToDo/src/main/java/com/masai/entity/Task.java
@@ -0,0 +1,29 @@
+package com.masai.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private String priority;
+    private LocalDate dueDate;
+    private boolean completed;
+
+}
