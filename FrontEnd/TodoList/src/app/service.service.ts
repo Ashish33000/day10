0a374b932b1f7ad68950be366a627e0da068e4c3
Ashish Kumar <ashishbhagat330@gmail.com>
2024-04-17 18:18:39 +0530

    added

--- FrontEnd/TodoList/src/app/service.service.ts
+++ FrontEnd/TodoList/src/app/service.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  apiurl='http://localhost:8080'
+  // signup
+  signup(data:any):Observable<any>{
+
+    return this.http.post(`${this.apiurl}/auth/register`,data);
+
+  }
 }
--- FrontEnd/TodoList/src/app/signup/signup.component.ts
+++ FrontEnd/TodoList/src/app/signup/signup.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ServiceService } from '../service.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  constructor(private service: ServiceService , private router: Router) {}
+  errmsg: any;
+  errmsgshow = false;
+  signupForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+
+  });
+  ngOnInit(): void {}
+  signupSubmit() {
+    if (this.signupForm.valid) {
+      //console.log(this.signupForm.value,'signupForm##');
+      this.errmsgshow = false;
+      //callapisignupHttpResponse<any>
+      this.service
+        .signup(this.signupForm.value)
+        .subscribe((res: HttpResponse<any>) => {
+          console.log(res.status, 'responsestatu##');
+          console.log(res, 'resilt');
+          if (res.status == 201) {
+            this.router.navigate(['signIn']);
+          } else {
+            this.errmsgshow = true;
+          }
+        });
+    } else {
+      this.errmsgshow = true;
+      this.errmsg = 'All field required!!';
+    }
+  }
 
 }
